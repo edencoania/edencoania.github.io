19cdcefb750f05f2cd3b98bf068d0d1fd18c17ac
Eden Coania <edencoania@gmail.com>
2023-05-10 15:33:39 +0300

    deploy app

--- bobbb/src/App.js
+++ bobbb/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Route, Routes,useParams} from 'react-router';
+import {BrowserRouter} from 'react-router-dom';
+import {MannageEntry} from './components/entry';
+import ShowUsers from './components/users';
+import {ShowTeams,Team} from './components/teams';
+import { ShowEvents,Event } from './components/events';
+import { User } from './components/users';
 import './App.css';
 
+//import Users from './components/users';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='App'>
+    <BrowserRouter>
+  <Routes >
+    <Route path="/" element={<MannageEntry />} />
+    <Route path="/users" element={<ShowUsers />} />
+    <Route path="/users/:userId" element={<User />} />
+    <Route path="/teams" element={<ShowTeams />} />
+    <Route path="/teams/:teamId" element={<Team  />} />
+    <Route path="/events" element={<ShowEvents/>}/>
+    <Route path="/events/:eventId" element={<Event/>}/>
+  </Routes>
+</BrowserRouter>
+</div>
   );
 }
 
